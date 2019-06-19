9a0af14adacfb47ba3101ce3941c6db867aac367
AaronCoplan <afcoplan@gmail.com>
2019-06-19 15:51:46 -0400

    Added initial files from `gradle init`

---
+++ src/main/java/com/aaroncoplan/todoist/Library.java
@@ -0,0 +1,7 @@
+package com.aaroncoplan.todoist;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/com/aaroncoplan/todoist/LibraryTest.java
@@ -0,0 +1,11 @@
+package com.aaroncoplan.todoist;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
