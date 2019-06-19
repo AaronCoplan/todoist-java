9e5850659cdd70fcbf18156023e0cb6843b4cf97
AaronCoplan <afcoplan@gmail.com>
2019-06-19 18:47:33 -0400

    Working on HTTP client.

--- src/main/java/com/aaroncoplan/todoist/Todoist.java
+++ src/main/java/com/aaroncoplan/todoist/Todoist.java
@@ -1,17 +1,40 @@
 package com.aaroncoplan.todoist;
 
-import jdk.incubator.http.HttpClient;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.time.Duration;
 
 public class Todoist {
 
-    private final String URL_BASE = "https://beta.todoist.com/API/v8/";
+    private final String URL_BASE = "https://beta.todoist.com/API/v8";
     private final String token;
 
     public Todoist(String token) {
         this.token = token;
     }
 
-    public boolean someLibraryMethod() {
-        return true;
+    public void doTest() {
+        var client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> chain.proceed(chain.request()
+                        .newBuilder()
+                        .addHeader("Authorization", String.format("Bearer %s", token))
+                        .addHeader("Accept", "application/json")
+                        .build()))
+                .followRedirects(false)
+                .callTimeout(Duration.ofSeconds(20))
+                .build();
+
+        var request = new Request.Builder()
+                .get()
+                .url(URL_BASE + "/projects")
+                .build();
+
+        try {
+            var response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/aaroncoplan/todoist/TodoistTest.java
+++ src/test/java/com/aaroncoplan/todoist/TodoistTest.java
@@ -1,11 +1,12 @@
 package com.aaroncoplan.todoist;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TodoistTest {
     @Test public void testSomeLibraryMethod() {
-        Todoist classUnderTest = new Todoist();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        var todoist = new Todoist("TOKEN");
+        todoist.doTest();
+        Assert.assertFalse(true);
     }
 }
