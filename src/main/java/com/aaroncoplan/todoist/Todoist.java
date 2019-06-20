956f5cef3d919d7cf51266b2e48ac6a7c6245eef
AaronCoplan <afcoplan@gmail.com>
2019-06-20 09:02:07 -0400

    Added ability to reopen a task.

--- src/main/java/com/aaroncoplan/todoist/Todoist.java
+++ src/main/java/com/aaroncoplan/todoist/Todoist.java
@@ -153,4 +153,16 @@ public class Todoist {
             e.printStackTrace();
         }
     }
+
+    public void reopenTask(long id) {
+        try {
+            HttpResponse<String> response = Unirest.post(URL_BASE + "/tasks/" + id + "/reopen")
+                    .asString();
+            if(response.getStatus() != HTTP_OK_NO_CONTENT) {
+                throw new Exception("HTTP STATUS " + response.getStatus());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
