0beb406589f57421ecf2c1bb528ae19422eeb262
AaronCoplan <afcoplan@gmail.com>
2019-06-20 09:43:22 -0400

    Added data model for label.

---
+++ src/main/java/com/aaroncoplan/todoist/model/Label.java
@@ -0,0 +1,14 @@
+package com.aaroncoplan.todoist.model;
+
+public class Label {
+
+    public final long id;
+    public final String name;
+    public final int order;
+
+    public Label(long id, String name, int order) {
+        this.id = id;
+        this.name = name;
+        this.order = order;
+    }
+}
