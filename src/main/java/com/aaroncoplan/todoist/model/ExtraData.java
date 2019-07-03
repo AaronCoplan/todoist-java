694b5ab5a94c0192de6e27073ab8a425aabeb938
AaronCoplan <afcoplan@gmail.com>
2019-07-03 15:10:22 -0400

    Added fields to ExtraData.

--- src/main/java/com/aaroncoplan/todoist/model/ExtraData.java
+++ src/main/java/com/aaroncoplan/todoist/model/ExtraData.java
@@ -1,17 +1,25 @@
 package com.aaroncoplan.todoist.model;
 
+import com.squareup.moshi.Json;
+
 public class ExtraData {
 
     public final String content;
+    @Json(name = "due_date") public final String dueDate;
+    @Json(name = "last_due_date") public final String lastDueDate;
 
-    public ExtraData(String content) {
+    public ExtraData(String content, String dueDate, String lastDueDate) {
         this.content = content;
+        this.dueDate = dueDate;
+        this.lastDueDate = lastDueDate;
     }
 
     @Override
     public String toString() {
         return "ExtraData{" +
                 "content='" + content + '\'' +
+                ", dueDate='" + dueDate + '\'' +
+                ", lastDueDate='" + lastDueDate + '\'' +
                 '}';
     }
 }
